a708e124097ccaf93926be4c6dcd9fd4c588eb20
radleylewis <radley.lewis@gmail.com>
2019-02-11 13:49:53 +0100

    feat: updated photos

--- client/src/components/preferencesComponent/preferences.js
+++ client/src/components/preferencesComponent/preferences.js
@@ -5,18 +5,18 @@ import './preferences.css';
 
 class Preferences extends Component {
 
-  render() {
-    return (
-      <div className="newsSourceSettings" >
-        <div className="sourcesContainer">
-          <NewsSources />
-        </div>
-      </div>
-    )
-  }
+	render() {
+		return (
+			<div className="newsSourceSettings" >
+				<div className="sourcesContainer">
+					<NewsSources />
+				</div>
+			</div>
+		);
+	}
 }
 
 export default connect(
-  null,
-  null,
+	null,
+	null,
 )(Preferences);
